2365e2c44d2e2129a0fac9f69877a4465b2a5977
Abhishek <79770548+rana-repo@users.noreply.github.com>
2021-03-18 04:33:36 -0400

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -10,3 +10,11 @@ terraform {
 provider "aws" {
   region     = "us-east-1"
 }
+
+
+
+resource "aws_instance" "foo" {
+  ami           = "ami-038f1ca1bd58a5790" 
+  instance_type = "t2.micro"
+
+  }
